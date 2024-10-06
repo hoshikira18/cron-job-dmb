5be387d71376e59fc473d123048b203d47105841
Tran Van Khuyen <81480245+hoshikira18@users.noreply.github.com>
2024-10-07 02:49:14 +0700

    add dmb fetching

--- netlify/functions/dmb-cron/dmb-cron.js
+++ netlify/functions/dmb-cron/dmb-cron.js
@@ -1,12 +1,15 @@
-const { schedule } = require('@netlify/functions')
+const { schedule } = require("@netlify/functions");
 
 // To learn about scheduled functions and supported cron extensions,
 // see: https://ntl.fyi/sched-func
-module.exports.handler = schedule('* * * * *', async (event) => {
-  const eventBody = JSON.parse(event.body)
-  console.log(`Next function run at ${eventBody.next_run}.`)
+module.exports.handler = schedule("* * * * *", async (event) => {
+  const eventBody = JSON.parse(event.body);
+  console.log(`Next function run at ${eventBody.next_run}.`);
+  await fetch("https://dmbi-server.onrender.com/store/store").then(() => {
+    console.log("Fetched to dmb server!");
+  });
 
   return {
     statusCode: 200,
-  }
-})
+  };
+});
